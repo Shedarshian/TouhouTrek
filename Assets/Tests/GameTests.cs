6bac553a747851c6198b65a9e19da3d969f565a9
BJSY <bianjingshiyi@hotmail.com>
2019-09-20 14:55:53 +0800

    添加了反向使用事件，扣置事件，弃牌的测试。

--- Assets/Tests/GameTests.cs
+++ Assets/Tests/GameTests.cs
@@ -7,6 +7,7 @@ using UnityEngine.TestTools;
 
 using ZMDFQ;
 using ZMDFQ.Cards;
+using ZMDFQ.Effects;
 using ZMDFQ.PlayerAction;
 
 namespace Tests
@@ -145,6 +146,134 @@ namespace Tests
             Assert.AreEqual(4, game.Players[0].Size);
             yield break;
         }
+        [UnityTest]
+        public IEnumerator useEventReverseTest()
+        {
+            Game game = new Game();
+            game.Init(new GameOptions()
+            {
+                players = new Player[]
+                {
+                    new Player(0),
+                    new Player(1)
+                },
+                characterCards = game.createCards(new TestCharacter(), 20),
+                actionCards = game.createCards(new TestAction1(), 20),
+                officialCards = game.createCards(new TestOfficial(), 20),
+                eventCards = game.createCards(new TestEvent(), 20),
+                firstPlayer = 0,
+                shuffle = false,
+                initCommunitySize = 0,
+                initInfluence = 0,
+                chooseCharacter = true,
+                doubleCharacter = false
+            });
+            game.StartGame();
+            game.Answer(new ChooseHeroResponse() { PlayerId = 0, HeroId = 1 });
+            game.Answer(new ChooseHeroResponse() { PlayerId = 1, HeroId = 4 });
+            game.Answer(new SimpleResponse() { PlayerId = 0, CardId = 21 });
+            game.Answer(new SimpleResponse() { PlayerId = 0, CardId = 22 });
+            game.Answer(new EndTurnResponse() { PlayerId = 0 });//TODO:回合结束的设定也许需要修改，按道理来讲应该先进入事件结算。
+            game.Answer(new ChooseDirectionResponse() { PlayerId = 0, CardId = 61, IfForward = false });
+
+            Assert.AreEqual(0, game.Players[0].Size);
+            yield break;
+        }
+        [UnityTest]
+        public IEnumerator setEventTest()
+        {
+            Game game = new Game();
+            game.Init(new GameOptions()
+            {
+                players = new Player[]
+                {
+                    new Player(0),
+                    new Player(1)
+                },
+                characterCards = game.createCards(new TestCharacter(), 20),
+                actionCards = game.createCards(new TestAction1(), 20),
+                officialCards = game.createCards(new TestOfficial(), 20),
+                eventCards = game.createCards(new TestEvent(), 20),
+                firstPlayer = 0,
+                shuffle = false,
+                initCommunitySize = 0,
+                initInfluence = 0,
+                chooseCharacter = true,
+                doubleCharacter = false
+            });
+            game.StartGame();
+            game.Answer(new ChooseHeroResponse() { PlayerId = 0, HeroId = 1 });
+            game.Answer(new ChooseHeroResponse() { PlayerId = 1, HeroId = 4 });
+            game.Answer(new EndTurnResponse() { PlayerId = 0 });//TODO:回合结束的设定也许需要修改，按道理来讲应该先进入事件结算。
+            game.Answer(new ChooseDirectionResponse() { PlayerId = 0, CardId = 61, IfSet = true });
+            game.Answer(new ChooseSomeCardResponse() { PlayerId = 0, Cards = new List<int>() { 21, 22 } });
+
+            Assert.IsInstanceOf<TestEvent>(game.Players[0].SaveEvent);
+            Assert.AreEqual(61, game.Players[0].SaveEvent.Id);
+
+            game.Answer(new EndTurnResponse() { PlayerId = 1 });
+            game.Answer(new ChooseDirectionResponse() { PlayerId = 1, CardId = 62, IfSet = true });
+            game.Answer(new ChooseSomeCardResponse() { PlayerId = 1, Cards = new List<int>() { 23, 24 } });
+
+            Assert.IsInstanceOf<TestEvent>(game.Players[1].SaveEvent);
+            Assert.AreEqual(62, game.Players[1].SaveEvent.Id);
+
+            game.Answer(new SimpleResponse() { PlayerId = 0, CardId = 25 });
+            game.Answer(new EndTurnResponse() { PlayerId = 0 });
+            game.Answer(new ChooseDirectionResponse() { PlayerId = 0, CardId = 63, IfSet = true });
+
+            Assert.AreEqual(2, game.Players[0].Size);
+            Assert.IsInstanceOf<TestEvent>(game.Players[0].SaveEvent);
+            Assert.AreEqual(63, game.Players[0].SaveEvent.Id);
+
+            yield break;
+        }
+        [UnityTest]
+        public IEnumerator discardTest()
+        {
+            Game game = new Game();
+            game.Init(new GameOptions()
+            {
+                players = new Player[]
+                {
+                    new Player(0),
+                    new Player(1)
+                },
+                characterCards = game.createCards(new TestCharacter(), 20),
+                actionCards = game.createCards(new TestAction1(), 20),
+                officialCards = game.createCards(new TestOfficial(), 20),
+                eventCards = game.createCards(new TestEvent(), 20),
+                firstPlayer = 0,
+                shuffle = false,
+                initCommunitySize = 0,
+                initInfluence = 0,
+                chooseCharacter = true,
+                doubleCharacter = false
+            });
+            game.StartGame();
+            game.Answer(new ChooseHeroResponse() { PlayerId = 0, HeroId = 1 });
+            game.Answer(new ChooseHeroResponse() { PlayerId = 1, HeroId = 4 });
+            game.Answer(new SimpleResponse() { PlayerId = 0, CardId = 21 });
+            game.Answer(new EndTurnResponse() { PlayerId = 0 });//TODO:回合结束的设定也许需要修改，按道理来讲应该先进入事件结算。
+            game.Answer(new ChooseDirectionResponse() { PlayerId = 0, CardId = 61, IfForward = true });
+
+            Assert.AreEqual(2, game.Players[0].Size);
+            Assert.AreEqual(22, game.Players[0].ActionCards[0].Id);
+            Assert.AreEqual(25, game.Players[0].ActionCards[1].Id);
+            Assert.AreEqual(2, game.Players[0].ActionCards.Count);
+
+            game.Answer(new EndTurnResponse() { PlayerId = 1 });
+            game.Answer(new ChooseDirectionResponse() { PlayerId = 1, CardId = 62, IfSet = true });
+            game.Answer(new ChooseSomeCardResponse() { PlayerId = 1, Cards = new List<int>() { 23, 24 } });
+
+            Assert.AreEqual(1, game.Players[1].ActionCards.Count);
+            Assert.AreEqual(21, game.UsedActionDeck[0].Id);
+            Assert.AreEqual(23, game.UsedActionDeck[1].Id);
+            Assert.AreEqual(24, game.UsedActionDeck[2].Id);
+            Assert.AreEqual(3, game.UsedActionDeck.Count);
+
+            yield break;
+        }
         class TestCharacter : HeroCard
         {
             public override List<Skill> Skills { get; } = new List<Skill>(new Skill[] { });
@@ -175,8 +304,11 @@ namespace Tests
         {
             protected override Task doEffect(Game game, SimpleResponse useWay)
             {
-                game.Players.Find(p => p.Id == useWay.PlayerId).Size += 1;
-                return Task.CompletedTask;
+                return UseCard.NormalUse(game, useWay, this, (g, r) =>
+                {
+                    game.Players.Find(p => p.Id == useWay.PlayerId).Size += 1;
+                    return Task.CompletedTask;
+                });
             }
             protected override SimpleRequest useWay()
             {
