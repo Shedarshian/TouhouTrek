4db5c898054f5411e6074b4ed449b57770f72fb2
BJSY <bianjingshiyi@hotmail.com>
2019-10-09 20:47:40 +0800

    更新了API文档。

--- Assets/Tests/GameTests.cs
+++ Assets/Tests/GameTests.cs
@@ -8,11 +8,21 @@ using UnityEngine;
 using UnityEngine.TestTools;
 
 using ZMDFQ;
+using ZMDFQ.Cards;
 using ZMDFQ.Effects;
 using ZMDFQ.PlayerAction;
 
 namespace Tests
 {
+    public class OtherTests
+    {
+        [UnityTest]
+        public IEnumerator getTypeTest()
+        {
+            Assert.AreEqual(typeof(EV_E001), typeof(Card).Assembly.GetTypes().First(t => t.Name == nameof(EV_E001)));
+            yield break;
+        }
+    }
     public class GameTests
     {
         [UnityTest]
