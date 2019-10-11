b32ce1028d67c84caec020df0b5d423b9c33bb77
Shedarshian <shedarshian@gmail.com>
2019-10-11 19:47:16 +0800

    白嫖blanked

---
+++ Assets/Scripts/Core/Card/ActionCard/AT_N007.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZMDFQ.PlayerAction;
+
+namespace ZMDFQ.Cards
+{
+    /// <summary>
+    /// 白嫖：指定一名玩家，获得他的一张手牌。
+    /// </summary>
+    public class AT_007: ActionCard
+    {
+        protected override bool canUse(Game game, Request nowRequest, FreeUse useInfo, out NextRequest nextRequest)
+        {
+            nextRequest = null;
+            switch (nowRequest)
+            {
+                case UseLimitCard useLimitCard:
+                    return Effects.UseWayResponse.CheckLimit(game, useLimitCard, useInfo, ref nextRequest, this);
+                case FreeUseRequest freeUse:
+                    if (useInfo.PlayersId.Count < 1)
+                    {
+                        nextRequest = new HeroChooseRequest() { };
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+            }
+            return false;
+        }
+
+        public override async Task DoEffect(Game game, FreeUse useWay)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
